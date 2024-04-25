a40caef9e2ebd097e5987905143bdf99692b75f2
andrericao <andrericao@hotmail.com>
2024-04-24 22:50:04 -0300

    update

--- livros-books/LogicaDeProgramacao eAlgoritmosComJavascript/cap13/exem13_2/front-livros/src/App.jsx
+++ livros-books/LogicaDeProgramacao eAlgoritmosComJavascript/cap13/exem13_2/front-livros/src/App.jsx
@@ -1,12 +1,23 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MenuSuperior from "./components/MenuSuperior";
 import InclusaoLivros from "./components/InclusaoLivros";
+import ManutencaoLivros from "./components/ManutencaoLivros";
+import ResumoLivros from "./components/ResumoLivros"
+
 
 const App = () => {
 
   return (
     <>
-      <MenuSuperior />
-      <InclusaoLivros />
+      <BrowserRouter>
+        <MenuSuperior />   
+          <Routes>
+            <Route path="/" element={ <InclusaoLivros /> } />
+            <Route path="manut" element={ <ManutencaoLivros /> } />
+            <Route path="resumo" element={ <ResumoLivros /> } />
+          </Routes>
+        </BrowserRouter>
     </>
   );
 }
