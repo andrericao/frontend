96f97e2758775903d918e7836e036c0b56d11766
andrericao <andrericao@hotmail.com>
2024-04-30 21:41:20 -0300

    terminado livro Logica de Programacao e Algoritmos com JavaScript

--- livros-books/LogicaDeProgramacao eAlgoritmosComJavascript/cap13/exem13_2/front-livros/src/components/ManutencaoLivros.jsx
+++
@@ -1,7 +0,0 @@
-const ManutencaoLivros = () => {
-	return (
-		<h2>Manutenção</h2>
-	)
-};
-
-export default ManutencaoLivros;
--- livros-books/LogicaDeProgramacao eAlgoritmosComJavascript/cap13/exem13_2/front-livros/src/components/ResumoLivros.jsx
+++
@@ -1,7 +0,0 @@
-const ResumoLivros = () => {
-	return (
-		<h2>Resumo</h2>
-	);
-}
-
-export default ResumoLivros;
